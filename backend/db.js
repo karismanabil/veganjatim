801d4fb7857be6afdc568fe9740444e1fc3bdb98
Kharisma Nabil <kharismanabil.gmail.com>
2025-06-15 19:07:55 +0700

    add port in db.js

--- backend/db.js
+++ backend/db.js
@@ -4,11 +4,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.MYSQLHOST,
+  port: process.env.MYSQLPORT,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
 });
 
 db.connect((err) => {
@@ -20,3 +20,12 @@ db.connect((err) => {
 });
 
 export default db;
+
+
+// const db = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+// }); // save for the local development
